1ea07259d92981cb5558da42eed9719676364dec
Gabriel dos Anjos Senra <fullstackgabe@Gabriels-MacBook-Air.local>
2024-08-03 16:36:49 -0300

    banco de dados ficticio

--- servidor.js
+++ servidor.js
@@ -1,11 +1,24 @@
 const porta = 3003;
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const bd = require('./bd.js')
 
 // app.use(req, res, next) usar para qualquer endpoint
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 });
+    res.send(bd.getProdutos());
+});
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bd.getProduto(req.params.id));
+});
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bd.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 });
 
 app.listen(porta, () => {
