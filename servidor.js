2bee88495c79b35c654c6cdfbeaadd7643b54954
Gabriel dos Anjos Senra <fullstackgabe@Gabriels-MacBook-Air.local>
2024-08-03 17:12:45 -0300

    criando todos os metodos e consumindo, final

--- servidor.js
+++ servidor.js
@@ -1,26 +1,44 @@
-const porta = 3003;
-
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-const bd = require('./bd.js')
+const porta = 3003
+const bancodedados = require('./bancodedados')
 
-// app.use(req, res, next) usar para qualquer endpoint
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true 
+}))
+
 app.get('/produtos', (req, res, next) => {
-    res.send(bd.getProdutos());
-});
+    res.send(bancodedados.getProdutos())
+})
 
 app.get('/produtos/:id', (req, res, next) => {
-    res.send(bd.getProduto(req.params.id));
-});
+    res.send(bancodedados.getProduto(req.params.id))
+})
 
 app.post('/produtos', (req, res, next) => {
-    const produto = bd.salvarProduto({
-        nome: req.body.name,
+    const produto = bancodedados.salvarProduto({
+        nome: req.body.nome,
         preco: req.body.preco
     })
     res.send(produto)
-});
+})
+
+app.put('/produtos', (req, res, next) => {
+    const produto = bancodedados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancodedados.excluirProduto(req.params.id)
+    res.send(produto)
+})
 
 app.listen(porta, () => {
-    console.log(`Servidor está executando na porta ${porta}.`);
-});
+    console.log(`Servidor está executando na porta ${porta}.`)
+})
